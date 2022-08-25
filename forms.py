e2e0d1cdbc49fe1dc71644598787c1e6207a1085
Archival Ray Millar <millar.archie@gmail.com>
2022-08-26 00:13:38 +0800

    Removed Comment

--- forms.py
+++ forms.py
@@ -4,7 +4,6 @@ from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
 
-##WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
