fcb33309433bb493b2b393030df138783b279421
Archival Ray Millar <millar.archie@gmail.com>
2022-08-26 02:49:10 +0800

    Add Postgres Update requirements.txt

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ gravatar = Gravatar(
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
